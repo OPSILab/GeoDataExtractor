a88bb83d3d34d0f7470cd316179b82c27c05e7ce
Gerbinix <gabriele.gerbino@live.it>
2023-11-23 15:42:27 +0100

    add other CrossOrigin

--- src/main/java/eu/urbanage/GeoDataExtractor/controller/IDRAController.java
+++ src/main/java/eu/urbanage/GeoDataExtractor/controller/IDRAController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
-@CrossOrigin(origins = "https://geodata-extractor-ui.dev.ecosystem-urbanage.eu")
+@CrossOrigin(origins = {"https://geodata-extractor-ui.dev.ecosystem-urbanage.eu", "https://geodata-extractor-ui.ecosystem-urbanage.eu", "https://gisviewer.santander.dev.ecosystem-urbanage.eu", "https://gisviewer.santander.ecosystem-urbanage.eu"})
 @RestController
 @RequestMapping("/api/idra/")
 public class IDRAController {
