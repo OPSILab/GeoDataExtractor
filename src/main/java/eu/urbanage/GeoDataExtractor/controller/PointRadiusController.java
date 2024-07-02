ca008fd912bafb96e06efbffbb0cc5ffa4dde4fe
Gerbinix <gabriele.gerbino@live.it>
2024-07-02 15:21:18 +0200

    Update PointRadiusController.java

--- src/main/java/eu/urbanage/GeoDataExtractor/controller/PointRadiusController.java
+++ src/main/java/eu/urbanage/GeoDataExtractor/controller/PointRadiusController.java
@@ -16,12 +16,13 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"https://geodata-extractor-ui.dev.ecosystem-urbanage.eu", "https://geodata-extractor-ui.ecosystem-urbanage.eu", "https://gisviewer.santander.dev.ecosystem-urbanage.eu", "https://gisviewer.santander.ecosystem-urbanage.eu", "http://localhost:4200"})
+@CrossOrigin(origins = { "https://geodata-extractor-ui.dev.ecosystem-urbanage.eu",
+        "https://geodata-extractor-ui.ecosystem-urbanage.eu", "https://gisviewer.santander.dev.ecosystem-urbanage.eu",
+        "https://gisviewer.santander.ecosystem-urbanage.eu", "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/pointradiusdata")
 @Deprecated
 public class PointRadiusController {
-
 
     @Autowired
     private GeojsonService gs;
